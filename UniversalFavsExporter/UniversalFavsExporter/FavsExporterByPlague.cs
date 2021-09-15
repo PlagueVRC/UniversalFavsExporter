a39e0d28a66ce0a5c7c780b7017c857a4dd4e414
Plague <36628963+OFWModz@users.noreply.github.com>
2021-09-15 06:48:30 +0100

    Add Support For Aids ML Versions

--- UniversalFavsExporter/UniversalFavsExporter/FavsExporterByPlague.cs
+++ UniversalFavsExporter/UniversalFavsExporter/FavsExporterByPlague.cs
@@ -21,9 +21,21 @@ namespace UniversalFavsExporter
 {
     public class FavsExporterByPlague : MelonMod
     {
-        public override void VRChat_OnUiManagerInit()
+        public override void OnApplicationStart()
         {
+            MelonCoroutines.Start(VRChat_OnUiManagerInit());
+        }
+
+        public IEnumerator VRChat_OnUiManagerInit()
+        {
+            while (VRCUiManager.prop_VRCUiManager_0 == null)
+            {
+                yield return new WaitForSeconds(1f);
+            }
+
             MelonCoroutines.Start(DelayedUIInit());
+
+            yield break;
         }
 
         public IEnumerator DelayedUIInit()
@@ -72,7 +84,7 @@ namespace UniversalFavsExporter
                                 }
 
                                 var FilePath = Environment.CurrentDirectory + "\\ExportedFavs\\" +
-                                               Child.Find("Button/TitleText").GetComponent<Text>().text + ".json";
+                                               MakeValidFileName(Child.Find("Button/TitleText").GetComponent<Text>().text) + ".json";
 
                                 File.WriteAllText(FilePath, Json);
 
@@ -111,6 +123,43 @@ namespace UniversalFavsExporter
             MelonLogger.Msg("Init!");
 
             yield break;
+        }
+
+        static char[] _invalids;
+
+        /// <summary>Replaces characters in <c>text</c> that are not allowed in 
+        /// file names with the specified replacement character.</summary>
+        /// <param name="text">Text to make into a valid filename. The same string is returned if it is valid already.</param>
+        /// <param name="replacement">Replacement character, or null to simply remove bad characters.</param>
+        /// <param name="fancy">Whether to replace quotes and slashes with the non-ASCII characters ” and ⁄.</param>
+        /// <returns>A string that can be used as a filename. If the output string would otherwise be empty, returns "_".</returns>
+        public static string MakeValidFileName(string text, char? replacement = '_', bool fancy = true)
+        {
+            StringBuilder sb = new StringBuilder(text.Length);
+            var invalids = _invalids ?? (_invalids = Path.GetInvalidFileNameChars());
+            bool changed = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (invalids.Contains(c))
+                {
+                    changed = true;
+                    var repl = replacement ?? '\0';
+                    if (fancy)
+                    {
+                        if (c == '"') repl = '”'; // U+201D right double quotation mark
+                        else if (c == '\'') repl = '’'; // U+2019 right single quotation mark
+                        else if (c == '/') repl = '⁄'; // U+2044 fraction slash
+                    }
+                    if (repl != '\0')
+                        sb.Append(repl);
+                }
+                else
+                    sb.Append(c);
+            }
+            if (sb.Length == 0)
+                return "_";
+            return changed ? sb.ToString() : text;
         }
 
         internal enum PopupType
